a6a68eaf72b7d0415ac5333d605815566efcd800
Seulgigi <slki1205@naver.com>
2022-05-15 01:53:46 +0900

    !

--- main/views.py
+++ main/views.py
@@ -1,27 +1,51 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post
+from .models import Blog
 from django.utils import timezone
 
 
 def showmain(request) :
-    posts = Post.objects.all()
-    return render(request, 'main/mainpage.html', {'posts':posts})
+    blogs = Blog.objects.all()
+    return render(request, 'main/mainpage.html', {'blogs':blogs})
 
 def firstpage(request) :
     return render(request, 'main/firstpage.html')
 
+def posts(request):
+    blog = Blog.objects.all()
+    return render(request, 'main/posts.html', {'blog':blog})
+
 def detail(request, id):
-    post = get_object_or_404(Post, pk = id)
-    return render(request, 'main/detail.html', {'post':post})
+    blog = get_object_or_404(Blog, pk = id)
+    return render(request, 'main/detail.html', {'blog':blog})
 
 def new(request) :
     return render(request, 'main/new.html')
 
 def create(request):
-    new_post = Post()
-    new_post.title = request.POST['title']
-    new_post.writer = request.POST['writer']
-    new_post.pub_date = timezone.now()
-    new_post.body = request.POST['body']
-    new_post.save()
-    return redirect('detail',new_post.id)
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.pub_date = timezone.now()
+    new_blog.body = request.POST['body']
+    new_blog.image = request.FILES.get('image')
+    new_blog.save()
+    return redirect('main:detail',new_blog.id)
+
+def edit(request, id):
+    edit_blog = Blog.objects.get(id = id)
+    return render(request, 'main/edit.html', {'blog':edit_blog})
+
+def update(request, id):
+    update_blog = Blog.objects.get(id=id)
+    update_blog.title = request.POST['title']
+    update_blog.writer = request.POST['writer']
+    update_blog.pub_date = timezone.now()
+    update_blog.body = request.POST['body']
+    update_blog.image = request.FILES.get('image')
+    update_blog.save()
+    return redirect('main:detail', update_blog.id)
+
+def delete(request, id):
+    delete_blog = Blog.objects.get(id=id)
+    delete_blog.delete()
+    return redirect('main:posts')
