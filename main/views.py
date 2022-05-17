9dc4c111f52ea17c3a88d7b682bc46dfa5335a8a
Seulgigi <slki1205@naver.com>
2022-05-18 00:51:17 +0900

    !

--- main/views.py
+++ main/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Blog
+from .models import Post
 from django.utils import timezone
 
 
 def showmain(request) :
-    blogs = Blog.objects.all()
+    blogs = Post.objects.all()
     return render(request, 'main/mainpage.html', {'blogs':blogs})
 
 def firstpage(request) :
     return render(request, 'main/firstpage.html')
 
 def posts(request):
-    blog = Blog.objects.all()
+    blog = Post.objects.all()
     return render(request, 'main/posts.html', {'blog':blog})
 
 def detail(request, id):
-    blog = get_object_or_404(Blog, pk = id)
+    blog = get_object_or_404(Post, pk = id)
     return render(request, 'main/detail.html', {'blog':blog})
 
 def new(request) :
     return render(request, 'main/new.html')
 
 def create(request):
-    new_blog = Blog()
+    new_blog = Post()
     new_blog.title = request.POST['title']
     new_blog.writer = request.POST['writer']
     new_blog.pub_date = timezone.now()
@@ -32,11 +32,11 @@ def create(request):
     return redirect('main:detail',new_blog.id)
 
 def edit(request, id):
-    edit_blog = Blog.objects.get(id = id)
+    edit_blog = Post.objects.get(id = id)
     return render(request, 'main/edit.html', {'blog':edit_blog})
 
 def update(request, id):
-    update_blog = Blog.objects.get(id=id)
+    update_blog = Post.objects.get(id=id)
     update_blog.title = request.POST['title']
     update_blog.writer = request.POST['writer']
     update_blog.pub_date = timezone.now()
@@ -46,6 +46,6 @@ def update(request, id):
     return redirect('main:detail', update_blog.id)
 
 def delete(request, id):
-    delete_blog = Blog.objects.get(id=id)
+    delete_blog = Post.objects.get(id=id)
     delete_blog.delete()
     return redirect('main:posts')
